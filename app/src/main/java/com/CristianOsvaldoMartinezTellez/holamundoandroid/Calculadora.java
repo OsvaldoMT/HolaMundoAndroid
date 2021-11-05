284d5f15c5de06fc9a12d34abcbac71546733da7
Martinez Tellez Cristian Osvaldo 5IM9 <martinez.tellez.osvaldo@gmail.com>
2021-11-05 09:53:02 -0600

    Segunda version

---
+++ app/src/main/java/com/CristianOsvaldoMartinezTellez/holamundoandroid/Calculadora.java
@@ -0,0 +1,27 @@
+package com.CristianOsvaldoMartinezTellez.holamundoandroid;
+
+import java.io.Serializable;
+
+public class Calculadora implements Serializable
+{
+
+    public void suma()
+    {
+
+    }
+
+    public void resta()
+    {
+
+    }
+
+    public void multiplicacion()
+    {
+
+    }
+
+    public void division()
+    {
+
+    }
+}
